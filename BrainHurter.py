9098c041935e3c65143356f5a6d0880f81fb247a
Vincent Shelden <vincentshelden@icloud.com>
2023-12-20 17:07:44 -0700

    arawa

--- BrainHurter.py
+++ BrainHurter.py
@@ -102,9 +102,10 @@ while True:
                     tile_type = get_tile_type(player_x, player_y + movement_speed)
                     if tile_type != 1:
                         player_y += movement_speed
-                elif event.key == pygame.K_r:
-                        player_location_reset()
-                        genrate_map()
+                elif event.key == pygame.K_r: 
+                    bomb_charges = 3
+                    player_location_reset()
+                    genrate_map()
 
 
             #Pull up menu
